219c99c3c2306eb152c84a36f1a54d544eb547ac
zack10 <zacksmetz@yahoo.com>
2024-08-05 12:00:49 +0200

    cors updating

--- index.js
+++ index.js
@@ -9,23 +9,41 @@ dotenv.config();
 
 const port = 3000;
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("db connected"))
+  .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
-// Use CORS middleware
-app.use(
-  cors({
-    origin: "http://localhost:8081", // Allow requests from this origin
-  })
-);
+// Configure CORS to handle multiple origins
+const allowedOrigins = [
+  "http://localhost:8081", // Local development URL
+  "https://ecommerceapp-1.netlify.app", // Production URL
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+// Use CORS middleware with the options
+app.use(cors(corsOptions));
+
+// Body parser middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+// Routes
 app.use("/api/products", productRouter);
 
+// Start the server
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening on port ${process.env.PORT || port}!`)
 );
