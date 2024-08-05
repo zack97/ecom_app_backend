3e5435ed643e39db9c6f5e74d285e6f08c317baf
zack10 <zacksmetz@yahoo.com>
2024-08-05 10:48:32 +0200

    updating cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Import the cors package
 const app = express();
 const productRouter = require("./routes/products");
 
@@ -13,11 +14,18 @@ mongoose
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
 
+// Use CORS middleware
+app.use(
+  cors({
+    origin: "http://localhost:8081", // Allow requests from this origin
+  })
+);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api/products", productRouter);
 
 app.listen(process.env.PORT || port, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
+  console.log(`Example app listening on port ${process.env.PORT || port}!`)
 );
